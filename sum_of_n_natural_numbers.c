19978d4f947e15098122dff686c6c89f003a0d9e
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2026-02-19 23:46:35 +0530

    Added optimized formula method for sum calculation

--- sum_of_n_natural_numbers.c
+++ sum_of_n_natural_numbers.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int n, sum = 0;
+    int n, sum_loop = 0, sum_formula;
 
     printf("Enter a number: ");
     scanf("%d", &n);
 
+    // Loop method
     for(int i = 1; i <= n; i++) {
-        sum = sum + i;
+        sum_loop += i;
     }
 
-    printf("Sum of first %d natural numbers = %d\n", n, sum);
+    // Formula method
+    sum_formula = n * (n + 1) / 2;
+
+    printf("Sum using loop = %d\n", sum_loop);
+    printf("Sum using formula = %d\n", sum_formula);
 
     return 0;
 }
